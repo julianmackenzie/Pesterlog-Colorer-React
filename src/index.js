f475448a3434cc0936234ef4487b4ee6e1d28e81
julianmackenzie <julianbmackenzie@gmail.com>
2023-07-31 11:52:21 -0500

    Switched from Browser to Hash Router
    GitHub pages didn't like browser routers so we switched to a hash router, which fixed refreshes on that site. Left behind the browser router code in case I ever need to swap back.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+// uncomment and comment out import below to switch back to browser router
+/*
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+*/
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 
 import Header from './components/Header';
 import Process from './pages/Process';
 import Tutorial from './pages/Tutorial';
 import Version from './components/Version';
 
-
+// uncomment and comment out function definition below to switch back to browser router
+/*
 let basename = "/Pesterlog-Colorer-React";  // Assign subdirectory based on if it's on deployment or local
 
 const router = createBrowserRouter([
+*/
+const router = createHashRouter([
   {
     path: "/",
     element: [<Header key="header" />, <App key="bodyelem" />, <Version key="version" />]
@@ -29,9 +42,14 @@ const router = createBrowserRouter([
     path: "/pages/Tutorial",
     element: [<Header key="header" />, <Tutorial key="bodyelem" />, <Version key="version" />],
   },
-], {
+]
+// uncomment to switch back to browser router
+/*
+, {
   basename: basename,
-});
+}
+*/
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
